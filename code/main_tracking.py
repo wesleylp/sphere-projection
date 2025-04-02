b0673dbfc22b00c1beba3916329660920833e929
wesley <wesley.passos@smt.ufrj.br>
2025-04-02 14:58:30 -0300

    add ball tracking and logging

---
+++ code/main_tracking.py
@@ -0,0 +1,99 @@
+import cv2
+
+import time
+from image_processing import (
+    preprocessing,
+    detect_circles,
+    filter_circles,
+    plot_circles_on_image,
+)
+
+from constants import MINRADIUS, MAXRADIUS, PROGRAM_LABEL
+
+cam = cv2.VideoCapture(0)
+
+
+def main():
+    if not cam.isOpened():
+        print("Error: Could not open the camera.")
+        return
+
+    file = open("logging.txt", "w")
+    file.write("timestamp\tx\ty\tr\n")
+
+    # loop de calibração
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("Error while tryng to capture frame from webcam.")
+            break
+
+        img = preprocessing(frame)
+
+        circles = detect_circles(
+            img,
+            method=cv2.HOUGH_GRADIENT,
+            dp=0.9,  # Inverse ratio of the accumulator resolution to the image
+            # resolution
+            minDist=100,  # Minimum distance between the centers of the detected
+            # circles
+            param1=50,  # Higher threshold for the Canny edge detector
+            param2=30,  # Accumulator threshold for the circle centers at the
+            # detection stage
+            minRadius=MINRADIUS,  # Minimum circle radius
+            maxRadius=MAXRADIUS,  # Maximum circle radius. If <= 0, uses the
+            # maximum image dimension.
+        )
+
+        # postprocessing to filter out false positives since we only
+        # want to detect one circle.
+        # circles = filter_circles(circles)
+
+        if circles is not None and circles.shape[1] == 1:
+            x, y, r = circles[0, 0, :]
+            file.write(f"{time.time()}\t{x}\t{y}\t{r}\n")
+            frame = plot_circles_on_image(frame, circles)
+        else:
+            # If no circles are detected, we can skip the logging
+            file.write(f"{time.time()}\t{0}\t{0}\t{0}\n")
+            continue
+
+        key = cv2.waitKey(1)
+
+        if key == ord("q"):
+            file.close()
+            break
+
+        if key == ord("p"):
+            cv2.waitKey(0)
+
+        cv2.putText(
+            frame,
+            "Press 'p' for pause/unpause...",
+            (10, 440),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2,
+        )
+        cv2.putText(
+            frame,
+            "Press 'q' for quit...",
+            (10, 470),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 255, 0),
+            2,
+        )
+
+        cv2.imshow(PROGRAM_LABEL, frame)
+
+    # Release the capture and writer objects
+    cam.release()
+    # out.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+    print("Done")
---
+++ code/plot_position.py
@@ -0,0 +1,54 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+sns.set_theme(font_scale=1.7, rc={"text.usetex": True})
+sns.set_style("white")
+this_filepath = os.path.abspath(__file__)
+
+logging_filepath = os.path.join(os.path.dirname(this_filepath), "..", "logging.txt")
+
+
+with open(logging_filepath, "r") as f:
+    lines = f.readlines()
+    # Skip the first line (header)
+    lines = lines[1:]
+    data = []
+    for line in lines:
+        parts = line.strip().split("\t")
+        timestamp = float(parts[0])
+        x = float(parts[1])
+        y = float(parts[2])
+        r = float(parts[3])
+        data.append((timestamp, x, y, r))
+df = pd.DataFrame(data, columns=["timestamp", "x", "y", "r"])
+
+# Convert timestamp to seconds
+df["timestamp"] = df["timestamp"] - df["timestamp"].min()
+# Convert to milliseconds
+df["timestamp"] = df["timestamp"] * 1000
+
+# Remove rows with negative radius
+df = df[df["r"] > 0]
+
+# Remove rows with negative x or y
+df = df[(df["x"] >= 0) & (df["y"] >= 0)]
+
+# plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(df["timestamp"], df["x"], label="x", color="blue", marker="o")
+plt.plot(df["timestamp"], df["y"], label="y", color="green", marker="o")
+plt.plot(df["timestamp"], df["r"], label="r", color="red", marker="o")
+plt.xlabel("Timestamp (ms)")
+plt.ylabel("Value")
+plt.title("Position and Radius Over Time")
+plt.legend()
+plt.grid()
+
+plt.show()
+plt.savefig(
+    os.path.join(os.path.dirname(this_filepath), "position.png"),
+    bbox_inches="tight",
+)
